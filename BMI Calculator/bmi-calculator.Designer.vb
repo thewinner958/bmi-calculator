b4709c7e0c18bde10d66d1af6c4de554cf02936f
TheWinner <thewinner958@gmail.com>
2019-05-11 20:21:33 +0300

    added some design

--- BMI Calculator/bmi-calculator.Designer.vb
+++ BMI Calculator/bmi-calculator.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmBMI
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,101 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblWeight = New System.Windows.Forms.Label()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.lblBMIOutput = New System.Windows.Forms.Label()
+        Me.lblBMI = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(28, 29)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(169, 24)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Body Mass Index"
+        '
+        'lblWeight
+        '
+        Me.lblWeight.AutoSize = True
+        Me.lblWeight.Location = New System.Drawing.Point(78, 88)
+        Me.lblWeight.Name = "lblWeight"
+        Me.lblWeight.Size = New System.Drawing.Size(62, 13)
+        Me.lblWeight.TabIndex = 1
+        Me.lblWeight.Text = "Weight (kg)"
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Location = New System.Drawing.Point(78, 153)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(55, 13)
+        Me.lblHeight.TabIndex = 2
+        Me.lblHeight.Text = "Height (m)"
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(235, 85)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(100, 20)
+        Me.txtWeight.TabIndex = 3
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(235, 150)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 20)
+        Me.txtHeight.TabIndex = 4
+        '
+        'lblBMIOutput
+        '
+        Me.lblBMIOutput.AutoSize = True
+        Me.lblBMIOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBMIOutput.Location = New System.Drawing.Point(29, 215)
+        Me.lblBMIOutput.Name = "lblBMIOutput"
+        Me.lblBMIOutput.Size = New System.Drawing.Size(165, 18)
+        Me.lblBMIOutput.TabIndex = 5
+        Me.lblBMIOutput.Text = "Body Mass Index (BMI):"
+        '
+        'lblBMI
+        '
+        Me.lblBMI.AutoSize = True
+        Me.lblBMI.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBMI.ForeColor = System.Drawing.Color.Red
+        Me.lblBMI.Location = New System.Drawing.Point(232, 217)
+        Me.lblBMI.Name = "lblBMI"
+        Me.lblBMI.Size = New System.Drawing.Size(0, 16)
+        Me.lblBMI.TabIndex = 6
+        '
+        'frmBMI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(418, 293)
+        Me.Controls.Add(Me.lblBMI)
+        Me.Controls.Add(Me.lblBMIOutput)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.lblWeight)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "frmBMI"
+        Me.Text = "BMI Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblWeight As Label
+    Friend WithEvents lblHeight As Label
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents lblBMIOutput As Label
+    Friend WithEvents lblBMI As Label
 End Class
